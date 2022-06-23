32347a52d28ba701e8d3e210280b2a5f2d768230
Klara <lichtenthal.klara@gmail.com>
2022-06-23 11:40:35 +0200

    versehentlich im falschen Branch an den buttons im menü gearbeitet, inhaltlich: man kann jetzt ins Crdits und options Menü

--- Assets/Scripts/main_menu_controller.cs
+++ Assets/Scripts/main_menu_controller.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class main_menu_controller : MonoBehaviour
 {
+    public GameObject Menu;
+    public GameObject Options;
+    public GameObject Credits;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OpenMenu();
     }
 
     // Update is called once per frame
@@ -25,4 +28,25 @@ public class main_menu_controller : MonoBehaviour
 #endif
         Application.Quit();
     }
+    public void OpenOptions()
+    {
+        Menu.SetActive(false);
+        Options.SetActive(true);
+
+    }
+    public void OpenMenu()
+    {
+        Menu.SetActive(true);
+        Options.SetActive(false);
+        Credits.SetActive(false);
+
+    }
+    public void OpenCredits()
+    {
+        Menu.SetActive(false);
+        Credits.SetActive(true);
+
+    }
+
+
 }

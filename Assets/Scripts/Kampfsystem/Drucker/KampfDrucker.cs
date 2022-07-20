a2a3d5f7987750908d5ba363dcb1d8dbece9e787
DasKritzel <leo.euti@gmail.com>
2022-07-21 00:31:42 +0200

    Code To-Do Liste geupdated

--- Assets/Scripts/Kampfsystem/Drucker/KampfDrucker.cs
+++ Assets/Scripts/Kampfsystem/Drucker/KampfDrucker.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //TODO: UI und Sprites soll bei Kampfstart in die Szene reinsliden
-//TODO: Kampfaufrufe sollen verschiedene Gegner nach Namen aufrufen
 public class KampfDrucker : AEnemyBaseClass
 {
 
